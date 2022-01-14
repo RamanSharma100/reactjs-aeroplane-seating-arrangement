870d5effbf978563b4de87ee99f01f775d2ae99f
Raman Sharma <rs9463194297@gmail.com>
2022-01-14 17:26:49 +0530

    improved modularity

--- src/App.jsx
+++ src/App.jsx
@@ -25,89 +25,46 @@ const App = () => {
 
     // filling seats
     let filledSeatsNumber = 0;
-    let allSeats = [...Array(20)].map((r) =>
-      [...Array(10)].map((r) => ({ filled: false, number: null }))
+    let allSeats = [...Array(20)].map(() =>
+      [...Array(10)].map(() => ({ filled: false, number: null }))
     );
+    let row = [];
+
+    const fillSeat = (seatIndexInRow) => {
+      if (filledSeatsNumber < input && row[seatIndexInRow].filled === false) {
+        row[seatIndexInRow].filled = true;
+        row[seatIndexInRow].number = filledSeatsNumber + 1;
+        filledSeatsNumber++;
+        setFilledSeats((prevFilled) => prevFilled + 1);
+      }
+    };
 
     allSeats.map((seatRow, index) => {
-      let row = [...seatRow];
       row = seatRow.map((st) => st);
       if (filledSeatsNumber < input) {
         // fill middle seats
         // columnA
-        if (filledSeatsNumber < input && row[1].filled === false) {
-          row[1].filled = true;
-          row[1].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
+        fillSeat(1);
         // columnB;
-        if (filledSeatsNumber < input && row[4].filled === false) {
-          row[4].filled = true;
-          row[4].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
-
-        if (filledSeatsNumber < input && row[5].filled === false) {
-          row[5].filled = true;
-          row[5].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
-
+        fillSeat(4);
+        fillSeat(5);
         //columnC
-        if (filledSeatsNumber < input && row[8].filled === false) {
-          row[8].filled = true;
-          row[8].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
+        fillSeat(8);
 
         //filling window seats
         // columnA
-        if (filledSeatsNumber < input && row[0].filled === false) {
-          row[0].filled = true;
-          row[0].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
+        fillSeat(0);
         //columnC
-        if (filledSeatsNumber < input && row[9].filled === false) {
-          row[9].filled = true;
-          row[9].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
+        fillSeat(9);
 
         //filling alise seats
         //columnA
-        if (filledSeatsNumber < input && row[2].filled === false) {
-          row[2].filled = true;
-          row[2].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
+        fillSeat(2);
         //columnB
-        if (filledSeatsNumber < input && row[3].filled === false) {
-          row[3].filled = true;
-          row[3].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
-        if (filledSeatsNumber < input && row[6].filled === false) {
-          row[6].filled = true;
-          row[6].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
+        fillSeat(3);
+        fillSeat(6);
         //columnC
-        if (filledSeatsNumber < input && row[7].filled === false) {
-          row[7].filled = true;
-          row[7].number = filledSeatsNumber + 1;
-          filledSeatsNumber++;
-          setFilledSeats((prevFilled) => prevFilled + 1);
-        }
+        fillSeat(7);
 
         allSeats[index] = row;
       }
